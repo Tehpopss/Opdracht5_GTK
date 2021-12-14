e9c814ef9c6c25c43466686ae75d4b1e7dcfb06c
Tehpopss <aeson.delcroix@student.vives.be>
2021-12-14 14:26:58 +0100

    adding WiringPi

--- GTK.c
+++ GTK.c
@@ -1,11 +1,13 @@
 #include <gtk/gtk.h>
+#include <wiringPi.h>
 
 void main (int argc, char *argv[])
 {
- gtk_init (&argc, &argv);
- GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
- GtkWidget *btn = gtk_button_new_with_label ("Close window");
- gtk_container_add (GTK_CONTAINER (win), btn);
- gtk_widget_show_all (win);
- gtk_main ();
+    wiringPiSetup();
+    gtk_init (&argc, &argv);
+    GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    GtkWidget *btn = gtk_button_new_with_label ("Close window");
+    gtk_container_add (GTK_CONTAINER (win), btn);
+    gtk_widget_show_all (win);
+    gtk_main ();
 }
